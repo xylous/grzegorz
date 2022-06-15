6f52b9c4084aabba623051bb4ba13f1a32e975e9
xylous <xylous.e@gmail.com>
2022-06-15 21:45:38 +0300

    Add GPLv3 license boilerplates to every source file

--- grzegorz/generator.py
+++ grzegorz/generator.py
@@ -1,3 +1,18 @@
+# Copyright (c) 2022 xylous <xylous.e@gmail.com>
+#
+# This file is part of grzegorz.
+# grzegorz is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# grzegorz is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# grzegorz.  If not, see <https://www.gnu.org/licenses/>.
+
 from .word import Word, readfile, writefile
 import re
 import json
