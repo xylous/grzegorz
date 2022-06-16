0ba3cccc7b5b52a695a4113fa68731240598c3ce
xylous <xylous.e@gmail.com>
2022-06-16 10:48:36 +0300

    Add a minimal pair flashcard model for Anki

---
+++ grzegorz/anki_integration.py
@@ -0,0 +1,37 @@
+from .word import Word, readfile
+import genanki
+
+grzegorz_minpair_model = genanki.Model(
+    # Randomly generated Model ID.
+    1958583115,
+    'Grzegorz Minimal Pairs',
+    fields=[
+        {'name': 'Word 1 text'},
+        {'name': 'Word 1 audio'},
+        {'name': 'Word 1 IPA'},
+        {'name': 'Word 2 text'},
+        {'name': 'Word 2 audio'},
+        {'name': 'Word 2 IPA'},
+    ],
+    templates=[
+        {
+            'name': 'Card 1',
+            'qfmt': 'What do you hear?<br>{{Word 1 audio}}<br>{{Word 1 text}} OR {{Word 2 text}}',
+            'afmt': '{{FrontSide}}<hr id=answer>{{Word 1 text}}<br>{{Word 1 IPA}}',
+        },
+        {
+            'name': 'Card 2',
+            'qfmt': 'What do you hear?<br>{{Word 2 audio}}<br>{{Word 1 text}} OR {{Word 2 text}}',
+            'afmt': '{{FrontSide}}<hr id=answer>{{Word 2 text}}<br>{{Word 2 IPA}}',
+        },
+    ],
+    css="""
+.card {
+  font-family: arial;
+  font-size: 20px;
+  text-align: center;
+  color: black;
+  background-color: white;
+}
+    """,
+)
