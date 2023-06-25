b53c4606c0c25bb212a2ac625d1141fd17cbe593
xylous <xylous.e@gmail.com>
2023-06-25 16:29:34 +0300

    Refactor: rename and move functions into MinPairGenerator class

--- grzegorz/generator.py
+++ grzegorz/generator.py
@@ -30,6 +30,7 @@ class MinPairGenerator:
         keep_stress: bool,
     ) -> None:
         self.optimise = optimise
+        self.optimised_phone_pairs = INTERESTING_DIFFERENCES
         self.keep_phonemes = keep_phonemes
         self.keep_chronemes = keep_chronemes
         self.keep_stress = keep_stress
@@ -72,14 +73,24 @@ class MinPairGenerator:
         if not pair.first.phonology or not pair.last.phonology:
             return False
         # A minimal pair is kept if it has an interesting difference.
-        if self.keep_phonemes and has_phoneme_contrast(pair, self.optimise):
+        if self.keep_phonemes and self.check_phoneme_contrast(pair, self.optimise):
             return PHONEME_MINPAIR
-        elif self.keep_chronemes and has_chroneme_contrast(pair):
+        elif self.keep_chronemes and self.check_chroneme_contrast(pair):
             return CHRONEME_MINPAIR
-        elif self.keep_stress and has_stress_contrast(pair):
+        elif self.keep_stress and self.check_stress_contrast(pair):
             return STRESS_MINPAIR
         else:
             return NOT_MINPAIR
+
+    def check_optimised_phone_pair(self, s1: Phone, s2: Phone) -> bool:
+        """
+        Two sounds are interestingly different if they are likely to be confused
+        """
+        for diff in self.optimised_phone_pairs:
+            if s1.sound in diff and s2.sound in diff \
+                    and s1.sound != s2.sound and s1.long == s2.long:
+                return True
+        return False
 
     def print_human_readable_check(self, word1: Word, word2: Word) -> int:
         word1.print_human_readable()
@@ -97,91 +108,77 @@ class MinPairGenerator:
             print("not minimal pair")
         return verdict
 
-### Helper functions ###
+    def check_phoneme_contrast(self, pair: MinPair, optimise: bool) -> bool:
+        first = pair.first.phonology
+        last = pair.last.phonology
 
-def has_phoneme_contrast(pair: MinPair, optimise: bool) -> bool:
-    first = pair.first.phonology
-    last = pair.last.phonology
-
-    # we have to work with same number of syllables
-    if len(first) != len(last):
-        return False
-
-    syl_diffs = differences(first, last)
-    # abort if more (or less) than one syllable is different
-    if len(syl_diffs) != 1:
-        return False
-    syl_diffs = syl_diffs[0]
-
-    # get the number of phones different in the matched syllable
-    phones_diffs = differences(syl_diffs[0].contents, syl_diffs[1].contents)
-    if len(phones_diffs) != 1 or len(syl_diffs[0].contents) != len(syl_diffs[1].contents):
-        return False
-
-    # make sure that the differences between sounds are based on phonemes, and
-    # not chronemes
-    diff = phones_diffs[0]
-    return diff[0].long == diff[1].long and \
-            (not optimise or are_interestingly_different(diff[0], diff[1]))
-
-def has_chroneme_contrast(pair: MinPair) -> bool:
-    first = pair.first.phonology
-    last = pair.last.phonology
-
-    # we have to work with same number of syllables
-    if len(first) != len(last):
-        return False
-
-    syl_diffs = differences(first, last)
-    # abort if more (or less) than one syllable is different
-    if len(syl_diffs) != 1:
-        return False
-    syl_diffs = syl_diffs[0]
-
-    # get the number of phones different in the matched syllable
-    phones_diffs = differences(syl_diffs[0].contents, syl_diffs[1].contents)
-    if len(phones_diffs) != 1 or len(syl_diffs[0].contents) != len(syl_diffs[1].contents):
-        return False
-
-    # make sure that the differences between sounds is based on sound length
-    diff = phones_diffs[0]
-    return diff[0].long != diff[1].long and diff[0].sound == diff[1].sound
-
-def has_stress_contrast(pair: MinPair) -> bool:
-    first = pair.first.phonology
-    last = pair.last.phonology
-
-    # we have to work with same number of syllables
-    if len(first) != len(last):
-        return False
-
-    fst_stress = []
-    snd_stress = []
-    for i in range(0, len(first)):
-        if first[i].contents != last[i].contents:
+        # we have to work with same number of syllables
+        if len(first) != len(last):
             return False
-        fst_stress += first[i].stress
-        snd_stress += last[i].stress
 
-    return fst_stress != snd_stress
+        syl_diffs = differences(first, last)
+        # abort if more (or less) than one syllable is different
+        if len(syl_diffs) != 1:
+            return False
+        syl_diffs = syl_diffs[0]
 
-def strip_stress(sounds: list[str]) -> list[str]:
-    """Remove stress marks from a list of sounds"""
-    return [x for x in sounds if not x in ['.', 'ˈ', 'ˌ', '̯', '']]
+        # get the number of phones different in the matched syllable
+        phones_diffs = differences(syl_diffs[0].contents, syl_diffs[1].contents)
+        if len(phones_diffs) != 1 or len(syl_diffs[0].contents) != len(syl_diffs[1].contents):
+            return False
+
+        # make sure that the differences between sounds are based on phonemes, and
+        # not chronemes
+        diff = phones_diffs[0]
+        return diff[0].long == diff[1].long and \
+                (not optimise or self.check_optimised_phone_pair(diff[0], diff[1]))
+
+    def check_chroneme_contrast(self, pair: MinPair) -> bool:
+        first = pair.first.phonology
+        last = pair.last.phonology
+
+        # we have to work with same number of syllables
+        if len(first) != len(last):
+            return False
+
+        syl_diffs = differences(first, last)
+        # abort if more (or less) than one syllable is different
+        if len(syl_diffs) != 1:
+            return False
+        syl_diffs = syl_diffs[0]
+
+        # get the number of phones different in the matched syllable
+        phones_diffs = differences(syl_diffs[0].contents, syl_diffs[1].contents)
+        if len(phones_diffs) != 1 or len(syl_diffs[0].contents) != len(syl_diffs[1].contents):
+            return False
+
+        # make sure that the differences between sounds is based on sound length
+        diff = phones_diffs[0]
+        return diff[0].long != diff[1].long and diff[0].sound == diff[1].sound
+
+    def check_stress_contrast(self, pair: MinPair) -> bool:
+        first = pair.first.phonology
+        last = pair.last.phonology
+
+        # we have to work with same number of syllables
+        if len(first) != len(last):
+            return False
+
+        fst_stress = []
+        snd_stress = []
+        for i in range(0, len(first)):
+            if first[i].contents != last[i].contents:
+                return False
+            fst_stress += first[i].stress
+            snd_stress += last[i].stress
+
+        return fst_stress != snd_stress
+
+### Helper functions ###
 
 def differences(A: list, B: list) -> list:
     """Given two lists, return pairs of elements that differ at the same index"""
     return [(a, b) for a, b in zip(A, B) if a != b]
-
-def are_interestingly_different(s1: Phone, s2: Phone) -> bool:
-    """
-    Two sounds are interestingly different if they are likely to be confused
-    """
-    for diff in INTERESTING_DIFFERENCES:
-        if s1.sound in diff and s2.sound in diff \
-                and s1.sound != s2.sound and s1.long == s2.long:
-            return True
-    return False
 
 def parse_differences_chain(diffs_chain: list[str]) -> list[tuple[str]]:
     """
